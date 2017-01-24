712ea3844c783f14d8e617cf9e6abe048ca2c15d
Jesse Vista <jesse@codegreencreative.com>
2017-01-24 16:15:22 -0500

    Pull in the certificate from the file

--- src/routes/routes.php
+++ src/routes/routes.php
@@ -3,7 +3,7 @@
 Route::get('saml/metadata', function () {
     $certificate = trim(str_replace([
         '-----BEGIN CERTIFICATE-----', '-----END CERTIFICATE-----'
-    ], '', config('samlidp.crt')));
+    ], '', Storage::get(config('samlidp.crt'))));
     $entity_id = config('samlidp.issuer_uri');
     $login = url(config('samlidp.login_uri'));
     return view('samlidp::metadata', compact('certificate', 'entity_id', 'login'));
