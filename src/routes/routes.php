ef38e7239c73aa1bf6a679980b177cf920d7f121
Jesse Vista <jesse@codegreencreative.com>
2017-01-24 16:32:18 -0500

    Use file_get_contents() instead of Storage facade

--- src/routes/routes.php
+++ src/routes/routes.php
@@ -3,7 +3,7 @@
 Route::get('saml/metadata', function () {
     $certificate = trim(str_replace([
         '-----BEGIN CERTIFICATE-----', '-----END CERTIFICATE-----'
-    ], '', Storage::get(config('samlidp.crt'))));
+    ], '', file_get_contents(config('samlidp.crt'))));
     $entity_id = config('samlidp.issuer_uri');
     $login = url(config('samlidp.login_uri'));
     return view('samlidp::metadata', compact('certificate', 'entity_id', 'login'));
