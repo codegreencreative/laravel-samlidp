28651b9f8e992e84f97078de582ad4666db30ac4
Jesse Vista <jesse@codegreencreative.com>
2017-01-31 15:07:15 -0500

    Use get not post

--- src/routes/routes.php
+++ src/routes/routes.php
@@ -2,4 +2,4 @@
 
 Route::get('saml/metadata', 'MetadataController@index');
 Route::get('saml/login', 'LoginController@showLoginForm')->name('saml.login');
-Route::post('saml/logout', 'LoginController@logout')->name('saml.logout');
+Route::get('saml/logout', 'LoginController@logout')->name('saml.logout');
