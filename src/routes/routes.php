8146a408c7182a907985060bf98f73451c480422
Jesse Vista <jesse@codegreencreative.com>
2017-01-31 15:45:44 -0500

    Added login route

--- src/routes/routes.php
+++ src/routes/routes.php
@@ -2,4 +2,5 @@
 
 Route::get('saml/metadata', 'MetadataController@index');
 Route::get('saml/login', 'LoginController@showLoginForm')->name('saml.login');
+Route::post('saml/login', 'LoginController@login');
 Route::get('saml/logout', 'LoginController@logout')->name('saml.logout');
