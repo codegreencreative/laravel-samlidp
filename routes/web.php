40c24ef7ff20461efd506053f11fcf7c3d416967
Jesse Vista <jvista@upwebdesign.com>
2019-03-12 17:12:40 -0400

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,3 @@
 <?php
 
 Route::resource('metadata', 'MetadataController')->only('index');
-// Route::resource('logout', 'LogoutController')->only('store');
-// Route::get('login', 'LoginController@showLoginForm')->name('saml.login');
-// Route::post('login', 'LoginController@login');
