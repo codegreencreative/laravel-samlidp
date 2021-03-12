64830792971d7f85561d668bb03a950dd2133607
Shawn Hooper <shawn@fivesense.ca>
2021-03-11 22:52:39 -0500

    Get E-Mail from field defined in the config (#53)
    Co-authored-by: Shawn Hooper <shawn@actionablebooks.com>

--- src/Events/Assertion.php
+++ src/Events/Assertion.php
@@ -29,7 +29,7 @@ class Assertion
     {
         $this->attribute_statement = &$attribute_statement;
         $this->attribute_statement
-            ->addAttribute(new Attribute(ClaimTypes::EMAIL_ADDRESS, auth()->user()->email))
+            ->addAttribute(new Attribute(ClaimTypes::EMAIL_ADDRESS, auth()->user()->__get(config('samlidp.email_field', 'email'))))
             ->addAttribute(new Attribute(ClaimTypes::COMMON_NAME, auth()->user()->name));
     }
 }
