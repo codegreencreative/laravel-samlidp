4a3e471de74724006b2f3a24483dd17f54d0594b
Jesse Vista <jvista@upwebdesign.com>
2023-02-28 09:15:19 -0500

    Update samlidp.php
    Add an option in config to set the NameID format.

--- config/samlidp.php
+++ config/samlidp.php
@@ -15,6 +15,8 @@ return [
     'email_field' => 'email',
     // Define the name field in the users table
     'name_field' => 'name',
+    // Defiine the NameID (optional)
+    // 'name_id_format' => 'urn:oasis:names:tc:SAML:1.1:nameid-format:emailAddress',
     // The URI to your login page
     'login_uri' => 'login',
     // Log out of the IdP after SLO
