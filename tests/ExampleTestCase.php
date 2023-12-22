8efd5015297adb63fd4f1e8a57f496ac6ea7b4fe
Jaianna Brooks <jaianna.brooks@oscillas.com>
2023-12-22 14:20:13 -0600

    Create basic test case to make sure orchestra package works properly

---
+++ tests/ExampleTestCase.php
@@ -0,0 +1,14 @@
+<?php declare(strict_types=1);
+
+namespace CodeGreenCreative\SamlIdp\Tests;
+
+use PHPUnit\Framework\Attributes\Test;
+
+class ExampleTestCase extends TestCase
+{
+    #[Test]
+    public function basic_test(): void
+    {
+        $this->assertTrue(true);
+    }
+}
