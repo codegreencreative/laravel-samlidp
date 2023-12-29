261661a7dc906039bb9d29a465d130a5b54cca99
Jaianna Brooks <jaianna.brooks@oscillas.com>
2023-12-29 13:09:51 -0600

    Use legacy laravel definitions instead of factory classes

--- database/factories/ServiceProviderFactory.php
+++ database/factories/ServiceProviderFactory.php
@@ -3,28 +3,17 @@
 namespace CodeGreenCreative\SamlIdp\Database\Factories;
 
 use CodeGreenCreative\SamlIdp\Models\ServiceProvider;
-use Illuminate\Database\Eloquent\Factories\Factory;
 use RobRichards\XMLSecLibs\XMLSecurityKey;
 
-class ServiceProviderFactory extends Factory
-{
-    protected $model = ServiceProvider::class;
 
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
-    public function definition(): array
-    {
-        return [
-            'destination_url' => $this->faker->url,
-            'logout_url' => $this->faker->url,
-            'certificate' => $this->faker->text(100),
-            'block_encryption_algorithm' => XMLSecurityKey::AES128_CBC,
-            'key_transport_encryption' => XMLSecurityKey::RSA_1_5,
-            'query_parameters' => false,
-            'encrypt_assertion' => false,
-        ];
-    }
-}
+$factory->define(ServiceProvider::class, function () {
+    return [
+        'destination_url' => $this->faker->url,
+        'logout_url' => $this->faker->url,
+        'certificate' => $this->faker->text(100),
+        'block_encryption_algorithm' => XMLSecurityKey::AES128_CBC,
+        'key_transport_encryption' => XMLSecurityKey::RSA_1_5,
+        'query_parameters' => false,
+        'encrypt_assertion' => false,
+    ];
+});
--- src/Models/ServiceProvider.php
+++ src/Models/ServiceProvider.php
@@ -2,14 +2,10 @@
 
 namespace CodeGreenCreative\SamlIdp\Models;
 
-use CodeGreenCreative\SamlIdp\Database\Factories\ServiceProviderFactory;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class ServiceProvider extends Model
 {
-    use HasFactory;
-
     protected $fillable = [
         'destination_url',
         'logout_url',
@@ -19,9 +15,4 @@ class ServiceProvider extends Model
         'query_parameters',
         'encrypt_assertion',
     ];
-
-    protected static function newFactory(): ServiceProviderFactory
-    {
-        return ServiceProviderFactory::new();
-    }
 }
