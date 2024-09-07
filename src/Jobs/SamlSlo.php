20e3d3dc1cb4d69a2747823d9905738f39c1d735
Jesse Vista <jvista@upwebdesign.com>
2024-09-07 09:23:10 -0400

    Refactor SamlSlo class for improved readability and maintainability

--- src/Jobs/SamlSlo.php
+++ src/Jobs/SamlSlo.php
@@ -2,29 +2,35 @@
 
 namespace CodeGreenCreative\SamlIdp\Jobs;
 
-use CodeGreenCreative\SamlIdp\Traits\PerformsSingleSignOn;
-use Illuminate\Foundation\Bus\Dispatchable;
 use LightSaml\Helper;
-use LightSaml\Model\Assertion\Issuer;
-use LightSaml\Model\Assertion\NameID;
-use LightSaml\Model\Context\DeserializationContext;
-use LightSaml\Model\Protocol\LogoutRequest;
-use LightSaml\Model\Protocol\LogoutResponse;
-use LightSaml\Model\Protocol\Status;
-use LightSaml\Model\Protocol\StatusCode;
-use LightSaml\Model\XmlDSig\SignatureWriter;
-use LightSaml\SamlConstants;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Str;
+use LightSaml\SamlConstants;
+use LightSaml\Model\Protocol\Status;
+use LightSaml\Model\Assertion\Issuer;
+use LightSaml\Model\Assertion\NameID;
+use LightSaml\Model\Protocol\StatusCode;
+use Illuminate\Foundation\Bus\Dispatchable;
+use LightSaml\Model\Protocol\LogoutRequest;
+use LightSaml\Model\Protocol\LogoutResponse;
+use LightSaml\Model\XmlDSig\SignatureWriter;
+use LightSaml\Model\Context\DeserializationContext;
+use CodeGreenCreative\SamlIdp\Traits\PerformsSingleSignOn;
 
 class SamlSlo
 {
-    use Dispatchable, PerformsSingleSignOn;
+    use Dispatchable;
+    use PerformsSingleSignOn;
 
     private $sp;
 
+    private $destination;
+
+    private $logout_request;
+
     /**
      * [__construct description]
+     *
      * @param [type] $sp [description]
      */
     public function __construct($sp)
@@ -35,6 +41,7 @@ class SamlSlo
 
     /**
      * [handle description]
+     *
      * @param  [type] $sp [description]
      * @return [type]     [description]
      */
@@ -53,17 +60,20 @@ class SamlSlo
             $deserializationContext = new DeserializationContext;
             $deserializationContext->getDocument()->loadXML($xml);
         }
+
         // Send the request to log out
         return $this->request();
     }
 
     /**
      * [response description]
+     *
      * @return [type] [description]
      */
     public function response()
     {
-        $this->response = (new LogoutResponse)->setIssuer(new Issuer($this->issuer))
+        $this->response = (new LogoutResponse)
+            ->setIssuer(new Issuer($this->issuer))
             ->setID(Helper::generateID())
             ->setIssueInstant(new \DateTime)
             ->setDestination($this->destination)
@@ -71,7 +81,9 @@ class SamlSlo
             ->setStatus(new Status(new StatusCode('urn:oasis:names:tc:SAML:2.0:status:Success')));
 
         if (config('samlidp.messages_signed')) {
-            $this->response->setSignature(new SignatureWriter($this->certificate, $this->private_key, $this->digest_algorithm));
+            $this->response->setSignature(
+                new SignatureWriter($this->certificate, $this->private_key, $this->digest_algorithm)
+            );
         }
 
         return $this->send(SamlConstants::BINDING_SAML2_HTTP_REDIRECT);
@@ -79,19 +91,22 @@ class SamlSlo
 
     /**
      * [request description]
+     *
      * @return [type] [description]
      */
     public function request()
     {
         $this->response = (new LogoutRequest)
             ->setIssuer(new Issuer($this->issuer))
-            ->setNameID((new NameID(Helper::generateID(), SamlConstants::NAME_ID_FORMAT_TRANSIENT)))
+            ->setNameID(new NameID(Helper::generateID(), SamlConstants::NAME_ID_FORMAT_TRANSIENT))
             ->setID(Helper::generateID())
             ->setIssueInstant(new \DateTime)
             ->setDestination($this->destination);
 
         if (config('samlidp.messages_signed')) {
-            $this->response->setSignature(new SignatureWriter($this->certificate, $this->private_key, $this->digest_algorithm));
+            $this->response->setSignature(
+                new SignatureWriter($this->certificate, $this->private_key, $this->digest_algorithm)
+            );
         }
 
         return $this->send(SamlConstants::BINDING_SAML2_HTTP_REDIRECT);
@@ -102,27 +117,26 @@ class SamlSlo
         $destination = $this->sp['logout'];
         $queryParams = $this->getQueryParams();
         if (!empty($queryParams)) {
-            if (!parse_url($destination, PHP_URL_QUERY)){
+            if (!parse_url($destination, PHP_URL_QUERY)) {
                 $destination = Str::finish(url($destination), '?') . Arr::query($queryParams);
-            }
-            else{
-                $destination .= '&'.Arr::query($queryParams);
+            } else {
+                $destination .= '&' . Arr::query($queryParams);
             }
         }
 
         $this->destination = $destination;
     }
 
-   private function getQueryParams()
-   {
-        $queryParams = (isset($this->sp['query_params']) ? $this->sp['query_params'] : null);
+    private function getQueryParams()
+    {
+        $queryParams = isset($this->sp['query_params']) ? $this->sp['query_params'] : null;
 
         if (is_null($queryParams)) {
             $queryParams = [
-                'idp' => config('app.url')
+                'idp' => config('app.url'),
             ];
         }
 
         return $queryParams;
-   }
+    }
 }
